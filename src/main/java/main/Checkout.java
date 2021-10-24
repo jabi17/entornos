88c63d9e82d9ed1e9f694687ef7e6ff439e5e94c
jabi17 <teamjp2003@gmail.com>
2021-10-24 20:48:11 +0200

    otrocambio

--- src/main/java/main/Checkout.java
+++ src/main/java/main/Checkout.java
@@ -4,6 +4,6 @@ public class Checkout {
     public static void main(String[] args) {
 
         System.out.println("adios mundo");
-        System.out.println("cambio");
+        System.out.println("cambio ");
     }
 }
